942834b3f55f2e531f3adc79ae445bc5b9ff3e6c
Spiro Stathakis <spiro@macbook>
2020-12-07 14:41:28 +0000

    Adding file sent from ACS to resolve issue

--- src/pages/solution.tsx
+++ src/pages/solution.tsx
@@ -16,168 +16,169 @@ import { classes as text } from "../styles/puma/text.st.css";
 import { st, classes } from "./solution.st.css";
 
 const Solution = ({ match, location }: any) => {
-  const params: any = useParams();
-  // console.log(match, location, params);
+    const params: any = useParams();
+    // console.log(match, location, params);
+    const catImgBaseUrl: string = "https://www.vocaleyes.org/uploads/1/";
+    // Set the initial content with what have from the link via link state.
+    const [content, setContent] = useState<any>({
+        name: location.state && location.state.title,
+        description: location.state && location.state.description,
+        image: location.state && location.state.media,
+    });
 
-  // Set the initial content with what have from the link via link state.
-  const [content, setContent] = useState<any>({
-    name: location.state && location.state.title,
-    description: location.state && location.state.description,
-    image: location.state && location.state.media,
-  });
+    const [caseStudies, setCaseStudies] = useState<any>([]);
 
-  const [caseStudies, setCaseStudies] = useState<any>([]);
-
-  useEffect(() => {
-    // GET solution via id from the url params.
-    api
-      .get(`/ideas/${params.solutionId}`)
-      .then(async (response) => {
-        const page = response.data.data[0];
-        // Update content with what comes back from the API
-        setContent({
-          name: page.name,
-          description: page.description,
-          image: page.images[0],
-          mainCategores: page.categories.main_categories.items || false,
-          orgTypes: page.categories.organisation_types.items || false,
-          links: page.links,
-        });
-
-        // Get and set case studies
-        page.links.news &&
-          api
-            .get(page.links.news)
+    useEffect(() => {
+        // GET solution via id from the url params.
+        api
+            .get(`/ideas/${params.solutionId}`)
             .then(async (response) => {
-              console.log(response.data.data);
-              return setCaseStudies(response.data.data);
+                const page = response.data.data[0];
+                // Update content with what comes back from the API
+                setContent({
+                    name: page.name,
+                    description: page.description,
+                    image: page.images[0],
+                    mainCategores: page.categories.main_categories.items || false,
+                    orgTypes: page.categories.organisation_types.items || false,
+                    links: page.links,
+                });
+
+                // Get and set case studies
+                page.links.news &&
+                api
+                    .get(page.links.news)
+                    .then(async (response) => {
+                        console.log(response.data.data);
+                        return setCaseStudies(response.data.data);
+                    })
+                    .catch((error) => {
+                        console.error(error);
+                    });
             })
             .catch((error) => {
-              console.error(error);
+                console.error(error);
             });
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, [params.solutionId]);
+    }, [params.solutionId]);
 
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
 
-  return (
-    <div className={st(classnames(classes.root, spacing.mb8))}>
-      <DefaultLayout>
-        <Banner
-          className={grid.edge}
-          style={{
-            marginBottom: "3vw",
-          }}
-          media={
-            content.image && (
-              <div
-                style={{
-                  opacity: 0.15,
-                  backgroundImage: `url(${content.image})`,
-                  backgroundSize: "cover",
-                  backgroundPosition: "0 50%",
-                }}
-              />
-            )
-          }
-        >
-          <H2 className={grid.goal} vol={8} uppercase>
+    return (
+        <div className={st(classnames(classes.root, spacing.mb8))}>
+            <DefaultLayout>
+                <Banner
+                    className={grid.edge}
+                    style={{
+                        marginBottom: "3vw",
+                    }}
+                    media={
+                        content.image && (
+                            <div
+                                style={{
+                                    opacity: 0.15,
+                                    backgroundImage: `url(${ catImgBaseUrl + (content.mainCategores && content.mainCategores[0].cat_image)})`,
+                                    backgroundSize: "cover",
+                                    backgroundPosition: "0 50%",
+                                }}
+                            />
+                        )
+                    }
+                >
+                    <H2 className={grid.goal} vol={8} uppercase>
             <span
-              className={classnames(
-                text.textBannerInline,
-                classes.category,
-                text.color2
-              )}
+                className={classnames(
+                    text.textBannerInline,
+                    classes.category,
+                    text.color2
+                )}
             >
               {content.mainCategores && content.mainCategores[0].cat_name}
             </span>
-            <br />
-            <span
-              className={classnames(
-                text.textBannerInline,
-                text.textBgSm,
-                classes.subCategory
-              )}
-            >
+                        <br />
+                        <span
+                            className={classnames(
+                                text.textBannerInline,
+                                text.textBgSm,
+                                classes.subCategory
+                            )}
+                        >
               {content.links && content.links.topic.name}
             </span>
-          </H2>
-        </Banner>
-        <Grid tag="main" variant={1} formatted>
-          <H1 vol={7}>
-            <small className={classnames(classes.solutionSub, text.color2)}>
-              Solution:
-            </small>
-            <br />
-            {content.name}
-          </H1>
-          <P vol={2}>
-            <strong>Relevant for: </strong>
-            {content.orgTypes &&
-              content.orgTypes.map((item: any, index: number) => {
-                const tail = index !== content.orgTypes.length - 1 && ", ";
-                return (
-                  <span key={`cattypes${index}`}>
+                    </H2>
+                </Banner>
+                <Grid tag="main" variant={1} formatted>
+                    <H1 vol={7}>
+                        <small className={classnames(classes.solutionSub, text.color2)}>
+                            Solution:
+                        </small>
+                        <br />
+                        {content.name}
+                    </H1>
+                    <P vol={2}>
+                        <strong>Relevant for: </strong>
+                        {content.orgTypes &&
+                        content.orgTypes.map((item: any, index: number) => {
+                            const tail = index !== content.orgTypes.length - 1 && ", ";
+                            return (
+                                <span key={`cattypes${index}`}>
                     {item.cat_name}
-                    {tail}
+                                    {tail}
                   </span>
-                );
-              })}
-          </P>
-          <ReactMarkdown
-            source={content.description}
-            renderers={renderers}
-            plugins={[gfm]}
-          />
-
-          <Grid
-            variant={2}
-            formatted
-            className={classnames(
-              spacing.mt2,
-              spacing.mt4,
-              spacing.mb8,
-              grid.pen
-            )}
-          >
-            <Link to="/solutions" className={buttons.link}>
-              Find more solutions
-            </Link>
-
-            <Link to="/suggestion" className={buttons.link}>
-              Suggest a solution
-            </Link>
-          </Grid>
-
-          <H2 className={classnames(grid.goal, spacing.mb2)} vol={7} uppercase>
-            Related Case studies
-          </H2>
-          {caseStudies.length > 0 && (
-            <div className={grid.goal}>
-              <Grid variant={4}>
-                {caseStudies.map((item: any) => {
-                  return (
-                    <Card
-                      title={item.title}
-                      url={`/case-studies/${item.key}/${slug(item.title)}`}
-                      description={item.description}
-                      media={item.images[0]}
-                      key={item.key}
+                            );
+                        })}
+                    </P>
+                    {content.image == null ? <div></div> : <img src={content.image} width='100%' height = '250px'></img>}
+                    <ReactMarkdown
+                        source={content.description}
+                        renderers={renderers}
+                        plugins={[gfm]}
                     />
-                  );
-                })}
-              </Grid>
-            </div>
-          )}
-        </Grid>
-      </DefaultLayout>
-    </div>
-  );
+
+                    <Grid
+                        variant={2}
+                        formatted
+                        className={classnames(
+                            spacing.mt2,
+                            spacing.mt4,
+                            spacing.mb8,
+                            grid.pen
+                        )}
+                    >
+                        <Link to="/solutions" className={buttons.link}>
+                            Find more solutions
+                        </Link>
+
+                        <Link to="/suggestion" className={buttons.link}>
+                            Suggest a solution
+                        </Link>
+                    </Grid>
+
+                    <H2 className={classnames(grid.goal, spacing.mb2)} vol={7} uppercase>
+                        Related Case studies
+                    </H2>
+                    {caseStudies.length > 0 && (
+                        <div className={grid.goal}>
+                            <Grid variant={4}>
+                                {caseStudies.map((item: any) => {
+                                    return (
+                                        <Card
+                                            title={item.title}
+                                            url={`/case-studies/${item.key}/${slug(item.title)}`}
+                                            description={item.description}
+                                            media={item.images[0]}
+                                            key={item.key}
+                                        />
+                                    );
+                                })}
+                            </Grid>
+                        </div>
+                    )}
+                </Grid>
+            </DefaultLayout>
+        </div>
+    );
 };
 
 export default Solution;
