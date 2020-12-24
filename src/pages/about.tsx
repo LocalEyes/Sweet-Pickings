68c814ff9aaf561b943fbbbcbd5fed0994a67d4b
acsdeveloper <developer@agilexcyber.in>
2020-12-24 12:37:57 +0530

    #53 - Update "About" page with same image in the header as on Suggest Content

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -18,11 +18,18 @@ const About = () => {
       </Helmet>
       <Banner
         className={grid.edge}
-        style={{
-          marginBottom: "3vw",
-        }}
+        media={
+          <div
+            style={{
+              opacity: 0.15,
+              backgroundImage: `url(https://ik.imagekit.io/tcvka0ufln/home_TUhFZjeTJ.jpg)`,
+              backgroundSize: "cover",
+              backgroundPosition: "0 50%",
+            }}
+          />
+        }
       >
-        <H1 vol={8} uppercase>
+       <H1 vol={8} uppercase>
           <span className={classnames(text.textBannerInline, text.textBg2)}>
             About
           </span>
