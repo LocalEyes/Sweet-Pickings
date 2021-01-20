49cae7809388709cd65c1e0f85d3b4763f6ba55a
agilecyberdeveloper <developer@agilecyber.solutions>
2021-01-20 16:13:48 +0530

    #9 - Keyword Search in the header

--- src/pages/search-results.tsx
+++ src/pages/search-results.tsx
@@ -2,25 +2,23 @@ import React, { useEffect, useState } from "react";
 import classnames from "classnames";
 import { Helmet } from "react-helmet-async";
 import { Link, useParams } from "react-router-dom";
-import { H1, H2, P, Grid } from "@actionishope/shelley";
 import Banner from "../components/Banner/Banner";
 import SocialShare from "../components/SocialSahre/SocialShare";
-import ChevRight from "../components/icons/ChevRight";
+import Card from "../components/Card/Card";
+import { H1,H2, H3, P, Grid } from "@actionishope/shelley";
+import { classes as spacing } from "@actionishope/shelley/styles/default/spacing.st.css";
 import { classes as grid } from "@actionishope/shelley/styles/default/grid.st.css";
 import { classes as text } from "../styles/puma/text.st.css";
-import { classes as spacing } from "../styles/puma/spacing.st.css";
 import { classes as buttons } from "../styles/puma/button.st.css";
 import { api, slug } from "../api";
-import Card from "../components/Card/Card";
-
+import ChevRight from "../components/icons/ChevRight";
 interface challengesProps {
   group: any;
   search: any;
 }
 
-const SearchResults = ({ group,search }: challengesProps) => {  
-// const SearchResults = () => {  
-  const [solutions, setSolutions] = useState<any>();
+const SearchResults = ({ group, search }: challengesProps) => {
+    const [solutions, setSolutions] = useState<any>();
   const [caseStudies, setCaseStudies] = useState<any>();
   const [challenges, setChallenges] = useState<any>();
   const params: any = useParams();
@@ -56,9 +54,9 @@ const SearchResults = ({ group,search }: challengesProps) => {
   }, [params.searchText]);
 
   return (
-    <div>
+    <div className={spacing.mb8}>
       <Helmet>
-        <title>Solutions database</title>
+        <title>Search Results - Solutions database</title>
       </Helmet>
       <Banner
         className={grid.edge}
@@ -73,30 +71,32 @@ const SearchResults = ({ group,search }: challengesProps) => {
           />
         }
       >
-        <H1 className={grid.goal} vol={8} uppercase>
+         <H1 className={grid.goal} vol={8} uppercase>
           <span className={text.textBg}>Create</span>
           <br />
-          <span className={text.textBg}>community</span>
+         <span className={text.textBg}>community</span>
           <br />
           <span className={text.textBg}>well-being</span>
         </H1>
-      </Banner>
-      <Grid tag="main" variant={1} formatted>
-        <div className={classnames(text.sectionLinkBanner, grid.edge)}>
-            <H2 className={classnames(text.color3)} vol={7} uppercase>
-                Search Results For:{params.searchText}
-            </H2>
-        </div>  
-        <div className={classnames(text.sectionLinkBanner, grid.edge)}>
-          <H2 className={classnames(text.color3)} vol={7} uppercase>
-            <Link to="/challenges">Challenges</Link> <ChevRight />
-          </H2>
-          {challenges ? (
-          <div className={classnames(grid.edge)}>
+       </Banner>
+       <br></br>
+       <Grid tag="main" variant={1}>
+          <H3 className={classnames(text.color2, grid.goal, spacing.mb2)}
+          vol={7}
+          uppercase>
+              Search Results For:{params.searchText}
+          </H3>
+       </Grid>
+       <SocialShare url={document.URL} title={'Search Results for: '+params.searchText}></SocialShare>
+      <Grid tag="main" variant={1}>
+        <H2 className={classnames(text.color2, grid.goal, spacing.mb2)} vol={7} uppercase>
+          <Link to="/challenges">Challenges</Link> <ChevRight />
+        </H2>
+        <div className={grid.goal}>
+        {challenges ? (
             <Grid variant={4}>
               {challenges ? (
-                challenges.map((item: any, index: number) => {
-                  
+                challenges.map((item: any) => {
                   return (
                     <Card
                       title={item.name}
@@ -106,14 +106,12 @@ const SearchResults = ({ group,search }: challengesProps) => {
                       key={item.key}
                     />
                   );
-                
                 })
               ) : (
-                  <P>No search results found!</P>
+                <P>No search results found!</P>
                 )}
             </Grid>
-          </div>
-        ):<P>No search results found!</P>}
+        ) : <P>No search results found!</P>}
         <Grid
             variant={2}
             formatted
@@ -131,17 +129,18 @@ const SearchResults = ({ group,search }: challengesProps) => {
               Suggest a challenge
             </Link>
             </Grid>
-        </div>
-        <div className={classnames(text.sectionLinkBanner, grid.edge)}>
-          <H2 className={classnames(text.color2)} vol={7} uppercase>
-            <Link to="/solutions">Solutions</Link> <ChevRight />
-          </H2>
-          {solutions ? (
-          <div className={grid.edge}>
+            </div>
+      </Grid>
+      {/* Solutions grid start */}
+      <Grid tag="main" variant={1}>
+        <H2 className={classnames(text.color2, grid.goal, spacing.mb2)} vol={7} uppercase>
+          <Link to="/solutions">Solutions</Link> <ChevRight />
+        </H2>
+        <div className={grid.goal}>
+        {solutions ? (
             <Grid variant={4}>
               {solutions ? (
-               solutions.map((item: any, index: number) => {
-                
+                solutions.map((item: any) => {
                   return (
                     <Card
                       title={item.name}
@@ -151,16 +150,12 @@ const SearchResults = ({ group,search }: challengesProps) => {
                       key={item.key}
                     />
                   );
-                
                 })
               ) : (
-                  <P>No search results found!</P>
+                <P>No search results found!</P>
                 )}
             </Grid>
-          </div>
-        ): 
-          <P>No search results found!</P>
-        }
+        ) : <P>No search results found!</P>}
         <Grid
             variant={2}
             formatted
@@ -171,41 +166,43 @@ const SearchResults = ({ group,search }: challengesProps) => {
               grid.pen
             )}
           >
-        <Link to="/solutions" className={buttons.link}>
-          Find more solutions
-        </Link>
+         <Link to="/solutions" className={buttons.link}>
+           Find more solutions
+         </Link>
 
-        <Link to="/suggestion/solution" className={buttons.link}>
-          Suggest a solution
-        </Link>
+         <Link to="/suggestion/solution" className={buttons.link}>
+           Suggest a solution
+         </Link>
         </Grid>
         </div>
-        <div className={classnames(text.sectionLinkBanner, grid.edge)}>
-          <H2 className={classnames(text.color1)} vol={7} uppercase>
-            <Link to="/case-studies">Case Studies</Link> <ChevRight />
-          </H2>
-          {caseStudies &&
-          <div className={grid.edge}>
+      </Grid>
+      {/* Solutions grid end */}
+      {/* CaseStudies grid start */}
+      <Grid tag="main" variant={1}>
+        <H2 className={classnames(text.color2, grid.goal, spacing.mb2)} vol={7} uppercase>
+          <Link to="/case-studies">Case Studies</Link> <ChevRight />
+        </H2>
+        <div className={grid.goal}>
+        {caseStudies ? (
             <Grid variant={4}>
-          {caseStudies ? (
-              caseStudies.map((item: any, index: number) => {
-                return (
-                  <Card
-                    title={item.title}
-                    url={`/case-studies/${item.key}/${slug(item.title)}`}
-                    description={item.description}
-                    media={item.images[0]}
-                    key={item.key}
-                  />
-                );
-              
-              })) :(
-                <P>No results</P>
-              )} 
-              </Grid>   
-            </div>  
-          }
-          <Grid
+              {caseStudies ? (
+                caseStudies.map((item: any) => {
+                  return (
+                    <Card
+                      title={item.title}
+                      url={`/case-studies/${item.key}/${slug(item.title)}`}
+                      description={item.description}
+                      media={item.images[0]}
+                      key={item.key}
+                    />
+                  );
+                })
+              ) : (
+                <P>No search results found!</P>
+                )}
+            </Grid>
+        ) : <P>No search results found!</P>}
+        <Grid
             variant={2}
             formatted
             className={classnames(
@@ -215,17 +212,17 @@ const SearchResults = ({ group,search }: challengesProps) => {
               grid.pen
             )}
           >
-           <Link to="/case-studies" className={buttons.link}>
-              Find more case studies
+            <Link to="/case-studies" className={buttons.link}>
+               Find more case studies
             </Link>
 
             <Link to="/suggestion/case-study" className={buttons.link}>
               Suggest a case study
             </Link>
-            </Grid>
+        </Grid>
         </div>
       </Grid>
-      <SocialShare title={'Home page'} url={document.URL}></SocialShare>
+      {/* CaseStudies grid end */}
     </div>
   );
 };
