29705754f9727e0c14fadec603b571529738c4c7
maasa-k <maasa.dev@gmail.com>
2021-02-28 14:51:34 -0800

    Add Barlow family font

--- src/components/Header/HeaderTwo.tsx
+++ src/components/Header/HeaderTwo.tsx
@@ -28,51 +28,25 @@ const Header = ({ className: classNameProp, children, ...rest }: GridProps) => {
       {/* {children} */}
       <nav className={grid.goal} >
         <div className={classes.flex_spaceBetween} >
-        {/* {isTabletOrMobile ? <div className={classes.burger_style} onClick={() => {setBurgerManager(!burgerManager)}}>
-          </div> : <ul className={classes.navList}>
-          <Text uppercase vol={4} as="li">
-            <Link className={classes.navItem} to="/">
-              Home
-            </Link>
-          </Text> */}
-
-          {/* <Text uppercase vol={4} as="li"> */}
             <Link className={classes.navItem} to="/challenges">
                 Grow for us, pick with us
             </Link>
-          {/* </Text> */}
 
-          {/* <Text uppercase vol={4} as="li"> */}
             <Link className={classes.navItem} to="/solutions">
                 United Kingdom, TXB273KE8
             </Link>
-          {/* </Text> */}
 
-          {/* <Text uppercase vol={4} as="li"> */}
             <Link className={classes.navItem} to="/case-studies">
                 help@sweetpickings.com
             </Link>
-          {/* </Text> */}
-        {/* </ul> }                  */}
+
+            {/* <Link></Link> */}
+
         <div className={classes.search_div}>
         </div>
         </div>
       </nav>
-      <br/>      
-      {/* <div className={classnames(grid.goal, classes.burger_options)} hidden={!burgerManager}>
-      <div>
-        <a href='/'>Home</a>          
-      </div>
-      <div>
-        <a href='/challenges'>Challenges</a>
-      </div>
-      <div>
-        <a href='/solutions'>Solutions</a>
-      </div>
-      <div className={classes.last_div}>
-        <a href='/case-studies'>Case Studies</a>
-      </div>
-      </div>      */}
+      <br/>
     </Grid>
     </div>
   );
